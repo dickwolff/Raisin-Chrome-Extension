8993419957e69785ab986f918d85328bb6afa622
Dick Wolff <dckwlff@gmail.com>
2024-05-31 12:13:42 +0200

    Add label to savings account

--- scripts/content.js
+++ scripts/content.js
@@ -30,6 +30,9 @@ const raisinAddon = async function () {
 
         const eurNumberFormat = new Intl.NumberFormat(customer.locale, { style: "currency", currency: "EUR" });
 
+        const dataFromLocalStorageString = localStorage.getItem("raisinAddon");
+        const dataFromLocalStorage = JSON.parse(dataFromLocalStorageString) || { accounts: [] };
+
         for (let idx = 0; idx < accountDivs.length; idx++) {
 
             const accountDiv = accountDivs[idx];
@@ -37,8 +40,11 @@ const raisinAddon = async function () {
 
             if (depositMatch) {
 
+                // Add shadow naming
+                addCustomName(accountDiv, depositMatch, dataFromLocalStorage);
+
                 // Add interest to glance view.
-                addInterestOverview(accountDiv, depositMatch, eurNumberFormat)
+                addInterestOverview(accountDiv, depositMatch, eurNumberFormat);
 
                 // Add interest to table.
                 addInterestToDetailsTable(accountDiv, depositMatch, eurNumberFormat);
@@ -47,11 +53,66 @@ const raisinAddon = async function () {
     }
 }
 
+function addCustomName(accountDiv, depositMatch, localStorageData) {
+    console.log(localStorageData)
+
+    let labelValue = null;
+
+    // Check if a label was set previously, if so store this.
+    const accountMatchIdx = localStorageData.accounts.findIndex(a => a.id === depositMatch.deposit_id);
+    console.log(accountMatchIdx, depositMatch.deposit_id)
+    if (accountMatchIdx > -1) {
+        labelValue = localStorageData.accounts[accountMatchIdx].name;
+    }
+
+    const chipSpan = createElement(
+        "span",
+        labelValue ? labelValue : "Geef een naam",
+        null,
+        null,
+        "overflow: hidden; text-overflow: ellipsis; padding-left: 11px; padding-right: 11px; white-space: nowrap;");
+
+    const chipDiv = createElement(
+        "div",
+        null,
+        `${depositMatch.deposit_id}-label`,
+        null,
+        "margin-left: 1rem; border-radius: 4px; height: 24px; display: inline-flex; align-items: center; justify-content: center; font-size: 0.8125rem; border: 1px solid #d7d7d7; " + (labelValue ? "background: #f5f5f5;" : "background: transparent"),
+        [chipSpan]);
+
+    // Add click handler.
+    chipDiv.onclick = () => {
+
+        // Create a promt for user to enter or update the name,.
+        const name = prompt(`Geef een naam op voor de rekening`, labelValue ? labelValue : "Bijvoorbeeld 'Sparen voor vakantie'");
+
+        // Verify the input.
+        if (name.trim().length < 3) {
+            alert("Naam moet meer dan 3 tekens zijn!");
+            return;
+        }
+
+        // Update local storage.
+        updateLocalStorage(localStorageData, depositMatch.deposit_id, name);
+
+        // Update the values on the screen.
+        chipSpan.innerHTML = name;
+        labelValue = name;
+        
+        // Update css.
+        chipDiv.setAttribute("style", "margin-left: 1rem; border-radius: 4px; height: 24px; display: inline-flex; align-items: center; justify-content: center; font-size: 0.8125rem; border: 1px solid #d7d7d7; " + (labelValue ? "background: #f5f5f5;" : "background: transparent;"));
+    }
+
+    accountDiv.firstChild.childNodes[1].setAttribute("style", "display: flex;");
+    accountDiv.firstChild.childNodes[1].appendChild(chipDiv);
+}
+
 function addInterestOverview(accountDiv, depositMatch, eurNumberFormat) {
 
     const quarterlyInterestSpan = createElement(
         "span",
         `Opgebouwde dit kwartaal: ${eurNumberFormat.format(parseFloat(depositMatch.total_accrued_interest_amount.denomination))}`,
+        null,
         null,
         "margin-left: 36px; margin-right: 1rem;");
 
@@ -61,6 +122,7 @@ function addInterestOverview(accountDiv, depositMatch, eurNumberFormat) {
 
     const lineDiv = createElement(
         "span",
+        null,
         null,
         "row styles_depositCardMain___3a-Kb",
         "justify-content: flex-start; padding: 0px 20px 15px 100px; font-size: 12px;",
@@ -82,17 +144,20 @@ function addInterestToDetailsTable(accountDiv, depositMatch, eurNumberFormat) {
                 const quarterlyInterestLabel = createElement(
                     "div",
                     "Opgebouwd dit kwartaal",
+                    null,
                     "col-sm-4 styles_detailsInfoRowTitle___3N3Fe",
                     null);
 
                 const quarterlyInterestValue = createElement(
                     "div",
                     eurNumberFormat.format(parseFloat(depositMatch.total_accrued_interest_amount.denomination)),
+                    null,
                     "col-sm-8 styles_detailsInfoRowText___1ZVyd",
                     null);
 
                 const quarterlyInterestRow = createElement(
                     "div",
+                    null,
                     null,
                     `row styles_detailsInfoRow___2YWtr ${depositMatch.deposit_id}-interest-row`,
                     null,
@@ -102,17 +167,20 @@ function addInterestToDetailsTable(accountDiv, depositMatch, eurNumberFormat) {
                 const totalInterestPaidLabel = createElement(
                     "div",
                     "Totaal uitbetaalde rente",
+                    null,
                     "col-sm-4 styles_detailsInfoRowTitle___3N3Fe",
                     null);
 
                 const totalInterestPaidValue = createElement(
                     "div",
                     eurNumberFormat.format(parseFloat(depositMatch.total_booked_interest_amount.denomination)),
+                    null,
                     "col-sm-8 styles_detailsInfoRowText___1ZVyd",
                     null);
 
                 const totalInterestPaidRow = createElement(
                     "div",
+                    null,
                     null,
                     `row styles_detailsInfoRow___2YWtr ${depositMatch.deposit_id}-interest-row`,
                     null,
@@ -127,10 +195,14 @@ function addInterestToDetailsTable(accountDiv, depositMatch, eurNumberFormat) {
     }
 }
 
-function createElement(type, innerHTML, className, style, children) {
+function createElement(type, innerHTML, id, className, style, children) {
 
     const element = document.createElement(type);
     element.innerHTML = innerHTML;
+
+    if (id) {
+        element.setAttribute("id", className);
+    }
 
     if (className) {
         element.setAttribute("class", className);
@@ -168,6 +240,27 @@ function waitForElement(selector) {
             subtree: true
         });
     });
+}
+
+function updateLocalStorage(currentData, accountId, newLabel) {
+
+    const accountMatchIdx = currentData.accounts.findIndex(a => a.id === accountId);
+
+    // If it does not exist, create a new record.
+    if (accountMatchIdx === -1) {
+        currentData.accounts.push({
+            id: accountId,
+            name: newLabel
+        });
+    }
+    else {
+
+        // Update the record.
+        currentData.accounts[accountMatchIdx].name = newLabel;
+    }
+
+    // Update localStorage.
+    localStorage.setItem("raisinAddon", JSON.stringify(currentData));
 }
 
 window.onload = () => raisinAddon();
