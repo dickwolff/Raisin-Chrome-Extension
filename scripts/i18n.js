fa9577e1a5dad50d06610071453dc1bf08b2a0c4
Dick Wolff <dckwlff@gmail.com>
2024-05-31 14:11:09 +0200

    Add more languages

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -8,6 +8,34 @@
         interestAccruedThisQuarter: "Opgebouwd dit kwartaal",
         totalInterestPaidOut: "Totaal uitbetaalde rente",
       },
+      de: {
+        giveNameChip: "Geben Sie einen Namen ein",
+        giveNameLabel: "Geben Sie einen Namen für das Konto ein",
+        giveNameLabelLengthError: "Name sollte mindestens 3 Zeichen sein!",
+        interestAccruedThisQuarter: "Diesem Viertel gebaut",
+        totalInterestPaidOut: "Gesamtzinsen bezahlt",
+      },
+      en: {
+        giveNameChip: "Enter a name",
+        giveNameLabel: "Enter a name for the account",
+        giveNameLabelLengthError: "Name should be at least 3 characters!",
+        interestAccruedThisQuarter: "Accrued this quarter",
+        totalInterestPaidOut: "Total interest paid",
+      },
+      fr: {
+        giveNameChip: "Entrez un nom",
+        giveNameLabel: "Entrez un nom pour le compte",
+        giveNameLabelLengthError: "Le nom doit comporter au moins 3 caractères!",
+        interestAccruedThisQuarter: "Construit ce trimestre",
+        totalInterestPaidOut: "Intérêt total payé",
+      },
+      es: {
+        giveNameChip: "Ingrese un nombre",
+        giveNameLabel: "Ingrese un nombre para la cuenta",
+        giveNameLabelLengthError: "¡El nombre debe ser al menos 3 caracteres!",
+        interestAccruedThisQuarter: "Acumulado este trimestre",
+        totalInterestPaidOut: "Intereses totales pagados",
+      },
     },
   };
 })();
