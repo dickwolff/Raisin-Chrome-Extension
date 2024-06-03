eebbd790ce7acfd434ab50b83eb74f6930f192e5
Dick Wolff <dckwlff@gmail.com>
2024-06-03 12:34:24 +0200

    Begun refactoring to TypeScript

---
+++ src/contentscript.ts
@@ -0,0 +1,15 @@
+
+class RaisinAddon {
+ 
+    public start() {
+
+    }
+}
+
+window.onload = () => {
+
+    console.log("Raisin add-on loaded!");
+    let ra = new RaisinAddon();
+
+    ra.start();
+}
