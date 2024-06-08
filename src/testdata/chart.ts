de03909e02153365f157121d6846ba6f7a8f6243
Dick Wolff <dckwlff@gmail.com>
2024-06-08 16:16:37 +0200

    Updated marketing material

---
+++ src/testdata/chart.ts
@@ -0,0 +1,34 @@
+
+export const chartTestData =
+    [
+        {
+            time: new Date("2024-02-01").toISOString().split("T")[0],
+            value: 20,
+            action: "PAY IN"
+        },
+        {
+            time: new Date("2024-04-01").toISOString().split("T")[0],
+            value: 20.13,
+            action: "INTEREST BOOKING"
+        },
+        {
+            time: new Date("2024-04-12").toISOString().split("T")[0],
+            value: 150.13,
+            action: "DEPOSIT"
+        },
+        {
+            time: new Date("2024-04-29").toISOString().split("T")[0],
+            value: 650.13,
+            action: "DEPOSIT"
+        },
+        {
+            time: new Date("2024-05-06").toISOString().split("T")[0],
+            value: 950.13,
+            action: "DEPOSIT"
+        },
+        {
+            time: new Date("2024-06-02").toISOString().split("T")[0],
+            value: 1210.13,
+            action: "DEPOSIT"
+        }
+    ]
