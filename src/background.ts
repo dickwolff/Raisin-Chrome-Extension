0fe1c86a6929b9221e69ea145c888c5eb7f77191
Dick Wolff <dckwlff@gmail.com>
2025-01-19 21:13:59 +0100

    Use webRequest to intercept, as that still works and is much better suited

--- src/background.ts
+++ src/background.ts
@@ -1,18 +1,25 @@
-chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((details) => {
 
-    // Get the account number from the URL.
-    const accountMatch = details.request.url.match(/BAC_(\d+)_\d+_\d+_\d+/);
+chrome.webRequest.onBeforeRequest.addListener((details) => {
 
-    // Send the account number to the content script.
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (isRaisinTab(tabs[0]?.url) && tabs[0]?.id) {
-            chrome.tabs.sendMessage(tabs[0].id, { accountNumber: accountMatch?.[0] });
+    // Check if the URL contains the account number.
+    if (details.url.indexOf("/customers/BAC_") > -1) {
+
+        // Get the account number from the URL.
+        const accountMatch = details.url.match(/BAC_(\d+)_\d+_\d+_\d+/);
+
+        // Send the account number to the content script.
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            if (isRaisinTab(tabs[0]?.url) && tabs[0]?.id) {
+                chrome.tabs.sendMessage(tabs[0].id, { accountNumber: accountMatch?.[0] });
+            }
+        });
+
+        function isRaisinTab(url: string | undefined): boolean {
+            return !!url && (url.indexOf("raisin") > -1 || url.indexOf("weltsparen") > -1);
         }
-    });
-
-    function isRaisinTab(url: string | undefined): boolean {
-        return !!url && (url.indexOf("raisin") > -1 || url.indexOf("weltsparen") > -1);
     }
-});
+}, { urls: ["https://api2.weltsparen.de/*"] });
 
-console.log("Raisin add-on background loaded!");
+chrome.runtime.onStartup.addListener(() => {
+    console.log("Raisin add-on background loaded!");
+});
