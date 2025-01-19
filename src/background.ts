a1385821e1fce1d645bb4e3248a65910f9213ac5
Dick Wolff <dckwlff@gmail.com>
2025-01-19 15:39:18 +0100

    Cleannup

--- src/background.ts
+++ src/background.ts
@@ -1,16 +1,18 @@
 chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((details) => {
 
-    console.log("Network request completed:", details.request);
-
     // Get the account number from the URL.
     const accountMatch = details.request.url.match(/BAC_(\d+)_\d+_\d+_\d+/);
 
     // Send the account number to the content script.
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs[0]?.id) {
+        if (isRaisinTab(tabs[0]?.url) && tabs[0]?.id) {
             chrome.tabs.sendMessage(tabs[0].id, { accountNumber: accountMatch?.[0] });
         }
     });
+
+    function isRaisinTab(url: string | undefined): boolean {
+        return !!url && (url.indexOf("raisin") > -1 || url.indexOf("weltsparen") > -1);
+    }
 });
 
 console.log("Raisin add-on background loaded!");
